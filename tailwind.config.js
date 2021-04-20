e7b9b02ff5b7d133421c41acc0586d7dd1f7a85b
EmielZuurbier <emielzuurbier@outlook.com>
2021-04-20 10:15:48 +0200

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,25 @@ module.exports = {
     mode: 'jit',
     purge: ['./index.html'],
     theme: {
+        container: {
+            center: true,
+            maxWidth: {
+                sm: '32em',
+                md: '40em',
+                lg: '48em',
+                xl: '64em'
+            }
+        },
         fontFamily: {
             serif: ['Bentham', 'serif']
         },
         extend: {
             colors: {
-                burgundy: '#710117',
-                cream: '#ffffff',
-                champagne: '#ECD5BB',
-                darkBlue: '#0B1C48'
+                merino: '#F4F1DE', // Cream
+                terracotta: '#E07A5F', // Red
+                gunPowder: '#3D405B', // Blue
+                acapulco: '#81B29A', // Green
+                manhattan: '#F2CC8F' // Yellow
             }
         }
     }
